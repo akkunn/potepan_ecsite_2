ec62fe76052c34a78fbcbaa2372fc67b463a8502
akkunn <853amtg@gmail.com>
2022-05-25 16:14:08 +0900

    product request specの追加

--- config/routes.rb
+++ config/routes.rb
@@ -26,8 +26,8 @@ mount SolidusPaypalCommercePlatform::Engine, at: '/solidus_paypal_commerce_platf
     get :about_us,                  to: 'sample#about_us'
     get :tokushoho,                 to: 'sample#tokushoho'
     get :privacy_policy,            to: 'sample#privacy_policy'
-    get 'products/:id',             to: 'products#show'
-    # resources :products,            only: [:show]
+    # get 'products/:id',             to: 'products#show'
+    resources :products,            only: [:show]
 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -2,16 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "Potepan::Products", type: :request do
   describe "#show" do
+    before do
+      get potepan_product_path(product.id)
+    end
     let(:product) { create(:product) }
     it "returns http success" do
-      # binding.pry
-      get :show, params: {id: product.id}
-      # get '/products/1' 
-      # get "/products/#{product.id}", params: {id: product.id}
-      # get "/products/#{product.id}"
-
       expect(response).to have_http_status(:success)
     end
-  end
 
+    it "have product.name" do
+      expect(response.body).to include(product.name)
+    end
+
+    it "have product.price" do
+      expect(response.body).to include(product.price.to_s)
+    end
+
+    it "have product.description" do
+      expect(response.body).to include(product.description)
+    end
+  end
 end
