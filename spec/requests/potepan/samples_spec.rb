887bec5e71f37390eb92581b3d58254580a71f28
akkunn <853amtg@gmail.com>
2022-05-27 08:27:54 +0900

    先頭の不要なスペースの削除、sample_specのpathの修正

--- spec/requests/potepan/samples_spec.rb
+++ spec/requests/potepan/samples_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Potepan::Samples", type: :request do
   describe "GET /potepan/index" do
     it "正常にレスポンスを返すこと" do
-      get "/potepan/index"
+      get potepan_index_path
       expect(response).to have_http_status(200)
     end
   end
