1c96cac382a957a3bf199bd1d5062f1865b403d0
akkunn <853amtg@gmail.com>
2022-05-30 18:59:52 +0900

    カテゴリーページの実装

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,4 +1,8 @@
 class Potepan::CategoriesController < ApplicationController
   def show
+    # binding.pry
+    @taxon = Spree::Taxon.find(params[:id])
+    @products = @taxon.all_products
+    @taxonomies = Spree::Taxonomy.all
   end
 end
--- spec/requests/potepan/categories_spec.rb
+++ spec/requests/potepan/categories_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Potepan::Categories", type: :request do
   describe "GET /show" do
+    let(:taxon) { create(:taxon) }
     it "returns http success" do
-      get "/potepan/categories/show"
+      get potepan_category_path(taxon.id)
       expect(response).to have_http_status(:success)
     end
   end
