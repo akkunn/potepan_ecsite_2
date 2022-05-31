e0fb7de7549084caf593d3612b32b77ccbc994d0
akkunn <853amtg@gmail.com>
2022-05-31 17:17:58 +0900

    request spec/categoriesの追加

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -3,6 +3,10 @@ class Potepan::CategoriesController < ApplicationController
     # binding.pry
     @taxon = Spree::Taxon.find(params[:id])
     @products = @taxon.all_products
+    # @products = Spree::Product.in_taxon(@taxon).includes(master: [:images, :default_price])
+    # @products = Spree::Product.in_taxon(@taxon)
+    # binding.pry
     @taxonomies = Spree::Taxonomy.all
+    # @taxonomies = Spree::Taxonomy.includes(:root)
   end
 end
--- spec/requests/potepan/categories_spec.rb
+++ spec/requests/potepan/categories_spec.rb
@@ -2,11 +2,36 @@ require 'rails_helper'
 
 RSpec.describe "Potepan::Categories", type: :request do
   describe "GET /show" do
+    # let(:taxon) { create(:taxon, parent_id: taxonomy.root.id, taxonomy: taxonomy) }
     let(:taxon) { create(:taxon) }
-    it "returns http success" do
+    let(:taxonomy) { create(:taxonomy) }
+    let(:product) { create(:product, taxons: [taxon]) }
+
+    before do
       get potepan_category_path(taxon.id)
+    end
+
+    it "returns http success" do
       expect(response).to have_http_status(:success)
     end
+
+    it "have taxon.name" do
+      expect(response.body).to include(taxon.name)
+    end
+
+    it "have taxonomy.name" do
+      expect(response.body).to include(taxonomy.name)
+    end
+
+    # it 'have product.name' do
+    #   binding.pry
+    #   products.each do |product|
+    #     expect(response.body).to include(product.name)
+    #   end
+    #     expect(response.body).to include(product.name)
+    # end
+
   end
+
 
 end
