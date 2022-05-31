b090588d6832f0a05f888fa75eb2b9204048fe0e
akkunn <853amtg@gmail.com>
2022-05-31 14:53:14 +0900

    feature spec moving success to index pageの追加

--- spec/features/potepan/categories_spec.rb
+++ spec/features/potepan/categories_spec.rb
@@ -4,23 +4,22 @@ RSpec.feature "Potepan::Categories", type: :feature do
   feature 'Categories page' do
     given(:taxon) { create(:taxon) }
     given(:taxonomy) { create(:taxonomy) }
-    given(:product) { create(:product, taxons: [taxon]) }
+    given(:products) { create(:product, taxons: [taxon]) }
 
     background do
       visit potepan_category_path(taxon.id)
     end
 
     scenario 'display categories page' do
-      # binding.pry
       expect(page).to have_selector 'h2', text: taxon.name
-      # binding.pry
-      # expect(page).to have_selector 'h5', text: product.name
       expect(page).to have_selector '.active', text: taxon.name
+      # expect(page).to have_selector 'h5', text: products.name
     end
 
-    # scenario 'moving success to products page' do
-    #   click_on product.name
-    #   expect(current_path).to eq potepan_product_path(product.id)
-    # end
+    scenario 'moving success to index page' do
+        find('.home').click
+        expect(current_path).to eq potepan_index_path
+    end
+
   end
 end
