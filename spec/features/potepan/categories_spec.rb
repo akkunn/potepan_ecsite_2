5f870bcf4b7b64677eb123fdcb9ca1c56e5b6ec5
akkunn <853amtg@gmail.com>
2022-05-31 20:38:16 +0900

    feature spec サイドバーのテストの追加

--- spec/features/potepan/categories_spec.rb
+++ spec/features/potepan/categories_spec.rb
@@ -4,16 +4,27 @@ RSpec.feature "Potepan::Categories", type: :feature do
   feature 'Categories page' do
     given(:taxon) { create(:taxon) }
     given(:taxonomy) { create(:taxonomy) }
-    given(:products) { create(:product, taxons: [taxon]) }
+    given(:product) { create(:product, taxons: [taxon]) }
+    given(:image) { create(:image) }
 
     background do
       visit potepan_category_path(taxon.id)
     end
 
     scenario 'display categories page' do
-      expect(page).to have_selector 'h2', text: taxon.name
-      expect(page).to have_selector '.active', text: taxon.name
-      # expect(page).to have_selector 'h5', text: products.name
+      within ".side-nav" do
+        expect(page).to have_content taxonomy.name
+      end
+      within ".lightSection.clearfix.pageHeader" do
+        expect(page).to have_selector 'h2', text: taxon.name
+        expect(page).to have_selector '.active', text: taxon.name
+      end
+    end
+
+    scenario 'product information' do
+      # binding.pry
+        expect(page).to have_content taxon.products.first.name
+
     end
 
     scenario 'moving success to index page' do
