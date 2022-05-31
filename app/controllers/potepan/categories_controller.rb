8be70c2a8fe07a32946ff4da4eea1d4d9f881dbb
akkunn <853amtg@gmail.com>
2022-05-31 20:59:55 +0900

    categories_controllerの修正

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,12 +1,12 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     # binding.pry
+    # @taxonomies = Spree::Taxonomy.all
+    @taxonomies = Spree::Taxonomy.includes(:root)
     @taxon = Spree::Taxon.find(params[:id])
-    @products = @taxon.all_products
+    @products = @taxon.all_products.includes(master: [:images, :default_price])
     # @products = Spree::Product.in_taxon(@taxon).includes(master: [:images, :default_price])
     # @products = Spree::Product.in_taxon(@taxon)
     # binding.pry
-    @taxonomies = Spree::Taxonomy.all
-    # @taxonomies = Spree::Taxonomy.includes(:root)
   end
 end
