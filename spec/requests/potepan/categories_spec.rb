80997de639e0c578b24b100ee55d62f3dba2de8c
akkunn <853amtg@gmail.com>
2022-06-02 10:53:41 +0900

    spec/feature/categoriesのdisplay categories page,move products page from category page in productBox class,move products page from category page in side-nav classのテストを追加

--- spec/requests/potepan/categories_spec.rb
+++ spec/requests/potepan/categories_spec.rb
@@ -3,10 +3,14 @@ require 'rails_helper'
 RSpec.describe "Potepan::Categories", type: :request do
   describe "GET /show" do
     let(:taxonomy) { create(:taxonomy) }
-    # let(:taxon) { create(:taxon, taxonomy: taxonomy, parent: root, name: "taxon") }
     let(:taxon) { create(:taxon, taxonomy: taxonomy) }
     let(:product) { create(:product, taxons: [taxon]) }
     let(:image) { create(:image) }
+    let(:filename) {
+      filename = image.attachment_blob.filename
+      "#{filename.base}#{filename.extension_with_delimiter}"
+    }
+    # let(:taxon) { create(:taxon, taxonomy: taxonomy, parent: root, name: "taxon") }
     # let(:root) { taxonomy.root }
 
     before do
@@ -27,11 +31,16 @@ RSpec.describe "Potepan::Categories", type: :request do
     end
 
     it "has correct product.name" do
+      # binding.pry
       expect(response.body).to include product.name
     end
 
     it "has correct product.price" do
       expect(response.body).to include product.display_price.to_s
+    end
+
+    it "has correct filename" do
+      expect(response.body).to include filename
     end
   end
 end
