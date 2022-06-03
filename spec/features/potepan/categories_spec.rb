8ebd3509faf0b1e99a1d3db349e1b1dc01d26a78
akkunn <853amtg@gmail.com>
2022-06-03 15:27:11 +0900

    features/potepan/categories_spec.rbのテストデータの手動セットの部分をbotに変更

--- spec/features/potepan/categories_spec.rb
+++ spec/features/potepan/categories_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.feature "Potepan::Categories", type: :feature do
   feature 'Categories page' do
-    given(:taxonomy) { create(:taxonomy, name: 'Categories') }
-    given(:taxon) { taxonomy.root }
-    given(:clothing) { create(:taxon, name: 'Clothing', taxonomy: taxonomy, parent: taxon) }
-    given(:shirts) { create(:taxon, name: 'Shirts', taxonomy: taxonomy, parent: clothing) }
-    given!(:bags) { create(:taxon, name: 'Bags', taxonomy: taxonomy, parent: taxon) }
-    given(:product1) { create(:product, name: 'Ruby Polo', price: 30, taxons: [shirts]) }
-    given(:product2) { create(:product, name: 'Solidus Tote', price: 20, taxons: [bags]) }
+    given(:taxonomy1) { create(:taxonomy) }
+    given(:taxonomy2) { create(:taxonomy) }
+    given(:root1) { taxonomy1.root }
+    given(:root2) { taxonomy2.root }
+    given(:taxon1) { create(:taxon, taxonomy: taxonomy1, parent: root1, name: 'taxon1') }
+    given!(:taxon2) { create(:taxon, taxonomy: taxonomy2, parent: root2) }
+    given(:product1) { create(:product, taxons: [taxon1]) }
+    given(:product2) { create(:product, taxons: [taxon2], price: 20) }
     given(:image1) { create(:image) }
     given(:image2) { create(:image) }
     given!(:filename) { image1.attachment_blob.filename }
@@ -17,17 +18,16 @@ RSpec.feature "Potepan::Categories", type: :feature do
     background do
       product1.images << image1
       product2.images << image2
-      visit potepan_category_path(shirts.id)
+      visit potepan_category_path(taxon1.id)
     end
 
     scenario 'display categories page' do
-      expect(page).to have_title "#{shirts.name} - BIGBAG Store"
+      expect(page).to have_title "#{taxon1.name} - BIGBAG Store"
       within ".side-nav" do
-        expect(page).to have_content taxonomy.name
-        expect(page).to have_content shirts.name
-        expect(page).to have_content shirts.products.count
-        expect(page).to have_content bags.name
-        expect(page).not_to have_content clothing.name
+        expect(page).to have_content taxonomy1.name
+        expect(page).to have_content taxon1.name
+        expect(page).to have_content taxon1.products.count
+        expect(page).to have_content taxon2.name
       end
       within ".productBox" do
         expect(page).to have_content product1.name
@@ -49,9 +49,9 @@ RSpec.feature "Potepan::Categories", type: :feature do
     end
 
     scenario 'move products page from category page in side-nav class' do
-      click_on bags.name
-      expect(current_path).to eq potepan_category_path(bags.id)
-      expect(page).to have_title "#{bags.name} - BIGBAG Store"
+      click_on taxon2.name
+      expect(current_path).to eq potepan_category_path(taxon2.id)
+      expect(page).to have_title "#{taxon2.name} - BIGBAG Store"
       expect(page).to have_content product2.name
       expect(page).to have_content product2.display_price.to_s
       expect(page).not_to have_content product1.name
