146dd3c56a7bb67c03658ca3edfb197b7c1d8182
akkunn <853amtg@gmail.com>
2022-06-10 16:36:50 +0900

    features/productsに関連商品が表示されているかのテストを追加

--- spec/features/potepan/products_spec.rb
+++ spec/features/potepan/products_spec.rb
@@ -5,9 +5,19 @@ RSpec.feature "Potepan::Products", type: :feature do
     let(:taxon) { create(:taxon) }
     let(:product) { create(:product, taxons: [taxon]) }
     let(:image) { create(:image) }
+    let(:related_products) { create_list(:product, 4, taxons: [taxon]) }
+    let(:related_products_images) { create_list(:image, 4) }
+    let(:other_related_products) { create_list(:product, 5, taxons: [taxon]) }
+    let(:other_related_products_images) { create_list(:image, 5) }
 
     background do
       product.images << image
+      [related_products, related_products_images].transpose.each do |related_product, related_products_image|
+        related_product.images << related_products_image
+      end
+      [other_related_products, other_related_products_images].transpose.each do |other_related_product, other_related_products_image|
+        other_related_product.images << other_related_products_image
+      end
       visit potepan_product_path(product.id)
     end
 
@@ -29,6 +39,21 @@ RSpec.feature "Potepan::Products", type: :feature do
       within '.breadcrumb' do
         click_link 'Home'
         expect(current_path).to eq potepan_index_path
+      end
+    end
+
+    scenario 'display related products' do
+      related_products.each do |related_product|
+        expect(page).to have_content related_product.name
+        expect(page).to have_content related_product.display_price.to_s
+        expect(page).to have_selector '.productBox', count: 4
+        click_link related_product.name, href: potepan_product_path(related_product.id)
+      end
+    end
+
+    scenario 'display other related products' do
+      other_related_products.each do |other_related_product|
+        expect(page).to have_selector '.productBox', count: 4
       end
     end
   end
