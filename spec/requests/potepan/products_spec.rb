0e1370ac3480b658d34a702bc68a464493ae87c5
akkunn <853amtg@gmail.com>
2022-06-10 16:54:54 +0900

    requests/productsの関連商品の表示のテストを追加

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -3,12 +3,17 @@ require 'rails_helper'
 RSpec.describe "Potepan::Products", type: :request do
   describe "#show" do
     before do
+      related_products.each_with_index do |related_product, i|
+        related_product.images << related_products_images[i]
+      end
       get potepan_product_path(product.id)
     end
 
     let(:taxonomy) { create(:taxonomy) }
     let(:taxon) { create(:taxon) }
     let(:product) { create(:product, taxons: [taxon]) }
+    let(:related_products) { create_list(:product, 4, taxons: [taxon]) }
+    let(:related_products_images) { create_list(:image, 4) }
 
     it "returns http success" do
       expect(response).to have_http_status(:success)
@@ -24,6 +29,13 @@ RSpec.describe "Potepan::Products", type: :request do
 
     it "have product.description" do
       expect(response.body).to include(product.description)
+    end
+
+    it "have related product" do
+      related_products.each do |related_product|
+        expect(response.body).to include(related_product.name)
+        expect(response.body).to include(related_product.display_price.to_s)
+      end
     end
   end
 end
