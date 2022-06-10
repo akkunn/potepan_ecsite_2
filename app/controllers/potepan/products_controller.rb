891fb5c01d6234e3b34221c73c1fee707ccede4a
akkunn <853amtg@gmail.com>
2022-06-10 19:55:06 +0900

    spree/products_decorator.rbの作成

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,6 +1,7 @@
 class Potepan::ProductsController < ApplicationController
+  COUNT_MAX_RELATED_PRODUCTS = 4
   def show
     @product = Spree::Product.find(params[:id])
-    @related_products = Spree::Product.in_taxons(@product.taxons).includes(master: [:default_price, :images]).where.not(id: @product.id).distinct.limit(4)
+    @related_products = @product.related_products.limit(COUNT_MAX_RELATED_PRODUCTS)
   end
 end
