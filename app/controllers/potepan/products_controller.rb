49ce2ec9dc70dcdc5443e6ecc08adea9d6bf53c5
akkunn <853amtg@gmail.com>
2022-06-13 09:35:50 +0900

    produc_decorator_specの追加

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,7 +1,6 @@
 class Potepan::ProductsController < ApplicationController
-  COUNT_MAX_RELATED_PRODUCTS = 4
   def show
     @product = Spree::Product.find(params[:id])
-    @related_products = @product.related_products.limit(COUNT_MAX_RELATED_PRODUCTS)
+    @related_products = @product.related_products.limit(4)
   end
 end
---
+++ spec/models/potepan/product_decorator_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Spree::ProductDecorator, type: :model do
+  describe "#related_products" do
+    let(:taxon) { create(:taxon) }
+    let(:product) { create(:product, taxons: [taxon]) }
+    let(:related_products) { create_list(:product, 4, taxons: [taxon]) }
+
+    it "returns related products" do
+      expect(product.related_products).to match_array(related_products)
+    end
+
+    it "returns related products not in product" do
+      expect(product.related_products).not_to include product
+    end
+
+    it "not uniq" do
+      expect(product.related_products == related_products.uniq).to eq true
+    end
+  end
+end
