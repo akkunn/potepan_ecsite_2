449137d66e5572090ec63d2a07caf0e078744312
akkunn <853amtg@gmail.com>
2022-06-13 12:37:23 +0900

    includes(master: [:default_price, :images])をcontrollerに切り出し

--- app/models/potepan/spree/product_decorator.rb
+++ app/models/potepan/spree/product_decorator.rb
@@ -2,7 +2,6 @@ module Spree::ProductDecorator
   def related_products
     Spree::Product.
       in_taxons(taxons).
-      includes(master: [:default_price, :images]).
       where.not(id: id).
       distinct
   end
