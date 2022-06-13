3656c2ecfde96c216e3584c11328b1bad4590a80
akkunn <853amtg@gmail.com>
2022-06-13 15:24:43 +0900

    長文になってしまったため改行

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,6 +2,7 @@ class Potepan::ProductsController < ApplicationController
   COUNT_MAX_RELATED_PRODUCTS = 4
   def show
     @product = Spree::Product.find(params[:id])
-    @related_products = @product.related_products.includes(master: [:default_price, :images]).limit(COUNT_MAX_RELATED_PRODUCTS)
+    @related_products = @product.related_products.includes(master: [:default_price, :images]).
+      limit(COUNT_MAX_RELATED_PRODUCTS)
   end
 end
