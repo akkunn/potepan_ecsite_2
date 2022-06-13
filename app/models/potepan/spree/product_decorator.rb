b95a2a9e08032556a8be2fd3d82e3655c9df935b
akkunn <853amtg@gmail.com>
2022-06-13 09:40:45 +0900

    インデントの調整

--- app/models/potepan/spree/product_decorator.rb
+++ app/models/potepan/spree/product_decorator.rb
@@ -1,6 +1,10 @@
 module Spree::ProductDecorator
   def related_products
-    Spree::Product.in_taxons(taxons).includes(master: [:default_price, :images]).where.not(id: id).distinct
+    Spree::Product.
+      in_taxons(taxons).
+      includes(master: [:default_price, :images]).
+      where.not(id: id).
+      distinct
   end
   Spree::Product.prepend self
 end
